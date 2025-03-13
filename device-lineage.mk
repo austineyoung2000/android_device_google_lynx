f3ccce117788371b23d864cf543be09c8406290f
Austin Young <austineyoung2000@gmail.com>
2025-03-12 22:48:22 -0400

    lynx: Shipping Google FaceUnlock & V4A

--- device-lineage.mk
+++ device-lineage.mk
@@ -20,6 +20,12 @@ PRODUCT_PACKAGES += \
 # PixelParts
 -include packages/apps/PixelParts/device.mk
 
+# Viper4Android FX
+-include packages/apps/ViPER4AndroidFX/config.mk
+
+# Face Unlock
+-include vendor/google/faceunlock/config.mk
+
 # PixelSupport
 include device/google/gs-common/pixelsupport/pixelsupport.mk
 
