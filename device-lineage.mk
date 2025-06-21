85962f0a5716d8782827e85a3e9710fa147a9267
Austin Young <austineyoung2000@gmail.com>
2025-06-21 00:15:02 -0400

    lynx: general fixup
    * Defined google face unlock due to source impl
    * Defined rising version
    
    Change-Id: Icae93d9afd49634d5c97ca92b0f2b0b8de519104

--- device-lineage.mk
+++ device-lineage.mk
@@ -20,11 +20,11 @@ PRODUCT_PACKAGES += \
 # PixelParts
 -include packages/apps/PixelParts/device.mk
 
-# Face Unlock
--include vendor/google/faceunlock/config.mk
-
 # PixelSupport
 include device/google/gs-common/pixelsupport/pixelsupport.mk
+
+# Ship Google Face Unlock ( GFU )
+TARGET_SUPPORTS_GFU := true
 
 # IWLAN
 PRODUCT_PACKAGES += \
